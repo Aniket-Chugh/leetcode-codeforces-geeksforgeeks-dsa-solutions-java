c684f400a804904695c935ec292477d9a574c46d
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2026-08-08 03:30:46 +0530

    Time: 4 ms (17.54%) | Memory: 43.8 MB (47.64%) - LeetSync

--- Array_leetcode/88-merge-sorted-array/merge-sorted-array.java
+++ Array_leetcode/88-merge-sorted-array/merge-sorted-array.java
@@ -1,12 +1,14 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-  for(int i=nums1.length-1;i>=n;i--){
-    nums1[i] = nums1[i-n];
-  }
 
-    for(int i=0;i<nums2.length;i++){
-    nums1[i] = nums2[i];
-  }
-Arrays.sort(nums1);
+        for (int i = nums1.length - 1; i >= n; i--) {
+            nums1[i] = nums1[i - n];
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            nums1[i] = nums2[i];
+        }
+
+        Arrays.sort(nums1);
     }
 }
